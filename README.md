2f0730b993cd1ed49496f3135cfc37a564ca0988
Frédéric Massart <fred@branchup.tech>
2022-10-23 17:58:53 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# `docusaurus-plugin-simple-analytics`
+
+[Simple Analytics](https://simpleanalytics.com/) plugin for Docusaurus.
+
+## Setup
+
+Install the package `@branchup/docusaurus-plugin-simple-analytics`:
+
+```
+yarn add @branchup/docusaurus-plugin-simple-analytics
+```
+
+Add the plugin to `docusaurus.config.js`:
+
+```
+plugins: [
+  ...
+  ['@branchup/docusaurus-plugin-simple-analytics', {}],
+  ...
+],
+```
+
+## Notes
+
+The plugin has no effect in development.
+
+## TODO
+
+- Support custom domain
+
+## License
+
+Licensed under the [MIT License](http://www.opensource.org/licenses/mit-license.php)
---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import type { LoadContext, Plugin } from '@docusaurus/types';
+
+export default function pluginSimpleAnalytics(context: LoadContext): Plugin {
+  const isProd = process.env.NODE_ENV === 'production';
+
+  return {
+    name: 'docusaurus-plugin-google-analytics',
+
+    injectHtmlTags() {
+      if (!isProd) {
+        return {};
+      }
+      return {
+        postBodyTags: [
+          {
+            tagName: 'script',
+            attributes: {
+              async: true,
+              defer: true,
+              src: 'https://scripts.simpleanalyticscdn.com/latest.js',
+            },
+          },
+          {
+            tagName: 'noscript',
+            innerHTML:
+              '<img src="https://queue.simpleanalyticscdn.com/noscript.gif" alt="" referrerpolicy="no-referrer-when-downgrade" />',
+          },
+        ],
+      };
+    },
+  };
+}
